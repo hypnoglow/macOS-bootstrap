a10258482e101db9a77e79bb39997472eb9e60cc
Igor Zibarev <zibarev.i@gmail.com>
2019-03-05 16:46:11 +0300

    [go] install go packages only if go environment is configured

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 go::get_packages() {
+    if [ ! -x "$(which go 2>/dev/null)" ]; then
+        echo "Go is not installed, skip installing go packages"
+	return
+    fi
+    if [ -z "${GOPATH:-}" ]; then
+        echo "GOPATH is not defined, skip installing go packages"
+	return
+    fi
+
     local packages=(
         github.com/msoap/go-carpet
         github.com/sqs/goreturns
-	golang.org/x/tools/cmd/goimports
+        golang.org/x/tools/cmd/goimports
     )
 
     for package in "${packages[@]}"; do

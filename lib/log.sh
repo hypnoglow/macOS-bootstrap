aa7a1bfe6f80f5a740714b4a7bf4b2fde625a771
Igor Zibarev <zibarev.i@gmail.com>
2023-07-09 22:02:39 +0300

    Minor improvement to log level output

--- lib/log.sh
+++ lib/log.sh
@@ -4,17 +4,17 @@
 ################################################################################
 
 log::debug() {
-  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[36m[DEBU]\e[0m $*" >&2
+  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[36m[DEBUG]\e[0m $*" >&2
 }
 
 log::info() {
-  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[34m[INFO]\e[0m $*" >&2
+  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[34m[ INFO]\e[0m $*" >&2
 }
 
 log::error() {
-  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[31m[ERRO]\e[0m $*" >&2
+  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[31m[ERROR]\e[0m $*" >&2
 }
 
 log::command() {
-  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[1;34m[COMM]\e[0m $*" >&2
+  echo -e "$(date -u +"%Y-%m-%dT%H:%M:%SZ") \e[1;34m[  CMD]\e[0m $*" >&2
 }

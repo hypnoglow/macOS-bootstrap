4ec24452a5e646348b69b56a0c488569cd33b8e8
Igor Zibarev <zibarev.i@gmail.com>
2023-08-17 16:02:56 +0300

    Rework go packages

---
+++ scripts/apps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apps::install_go_apps() {
+    if [ ! -x "$(which go 2>/dev/null)" ]; then
+        log::info "Go is not installed, skip installing go packages"
+        return
+    fi
+
+    local packages=(
+        github.com/msoap/go-carpet@v1.10.0
+        github.com/daixiang0/gci@v0.10.1
+    )
+
+    local dir="${HOME}/go/bin"
+
+    for package in "${packages[@]}"; do
+        log::command "--> GOBIN=${dir} go install -v ${package}"
+        GOBIN="${dir}" go install -v "${package}"
+    done
+}

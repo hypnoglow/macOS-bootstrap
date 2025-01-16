2feb31744a4bd9f8400d3ab2ee914476c51c0cbe
Igor Zibarev <zibarev.i@gmail.com>
2025-01-16 18:08:51 +0200

    :up: antares

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -8,7 +8,7 @@ apps::install_go_apps() {
 
     local packages=(
         github.com/msoap/go-carpet@v1.10.0
-        github.com/daixiang0/gci@v0.12.3
+        github.com/daixiang0/gci@v0.13.5
         github.com/quasilyte/qbenchstat/cmd/qbenchstat@latest
     )
 
